864098b64f2316492adeaa48ce3404f6279e2873
kkozv <kozlowska993@gmail.com>
2026-02-01 14:28:33 +0100

    Change background color in walentynki1.py

--- walentynki1.py
+++ walentynki1.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="💘", page_icon="💘", layout="centered")
 st.markdown(
     """
     <style>
-     html, body, [data-testid="stAppViewContainer"] { background: #FFF9D6 !important; }
+     html, body, [data-testid="stAppViewContainer"] { background: #fbffb4 !important; }
 
       .block-container { padding-top: 0.6rem; }
     </style>
